87fe0bf77a65edfe1a03f1c086859e97bcd1464d
Christian Melchior <cme@trifork.com>
2013-04-04 20:44:22 +0200

    Added demo using web workers

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 "use strict";
 
-var worker = new Worker('search.js');
 var cards = {};
+var worker = new Worker('search.js');
+var start;
+var workerTimes = [];
+var useWebWorkers = false;
+var useTransferableObjects = false;
 
 $(document).ready(function(){
+    initializeCheckboxListeners();
+
     $.getJSON("all_cards.json",
         function(data){
             cards = data;
@@ -14,15 +20,76 @@ $(document).ready(function(){
             });
         })
 
-    $('#search').bind('input', function() {
-        worker.addEventListener('message', function(e) {
-            $('#search_results').empty();
-            e.data.forEach(function(card) {
-                $('#search_results').append('<div>'+ card["name"] +'</div>');
-            });
-        }, false);
+    worker.addEventListener('message', function(e) {
+        filterCards(e.data);
+    }, false);
 
-        worker.postMessage({ 'search': $(this).val(), 'cards':cards}); // Send data to our worker.
+    $('#search').bind('input', function() {
+        start = new Date().getTime();
+        if (useWebWorkers) {
+            if (useTransferableObjects) {
+                worker.postMessage()
+
+                throw Error("Not implemented");
+            } else {
+                 worker.postMessage({ 'search': $(this).val(), 'cards':cards}); // Send data to our worker
+            }
+        } else {
+            doLocalSearch($(this).val(), cards);
+        }
    });
 });
 
+function initializeCheckboxListeners() {
+    $('#webworkers').click(function() {
+       useWebWorkers = $(this).is(':checked');
+    });
+
+    $('#transferableObjects').click(function() {
+       useTransferableObjects = $(this).is(':checked');
+    });
+}
+
+function calculateFilterTimes() {
+    var time = new Date().getTime() - start;
+    console.log(time);
+    workerTimes.push(time);
+
+    $("#last_run").text("Last filter: " + time + " ms.");
+
+    var avg = 0;
+    for (var i = 0; i < workerTimes.length; i++) {
+        avg += workerTimes[i] / workerTimes.length;
+    }
+
+    $("#avg_run").text("Average: " + avg + " ms.");
+}
+
+function doLocalSearch(val, cards) {
+    console.log("Local search");
+    var cards = cards;
+    var searchValue = val;
+    var result = [];
+
+    var i = 0;
+    cards.forEach(function(cardSet) {
+        cardSet['cards'].forEach(function (card) {
+            var regexp = new RegExp(".*?" + searchValue + ".*?" ,"gi");
+
+            if (card['name'].match(regexp)) {
+                result[i] = card;
+                i++;
+            }
+        });
+    });
+
+    filterCards(result);
+}
+
+function filterCards(cards) {
+    calculateFilterTimes();
+    $('#search_results').empty();
+    cards.forEach(function(card) {
+        $('#search_results').append('<div>'+ card["name"] +'</div>');
+    });
+}
--- search.js
+++ search.js
@@ -8,6 +8,27 @@ self.addEventListener('message', function(e) {
 
     var cards = e.data.cards;
     var searchValue = e.data.search;
+    self.postMessage(doSearch(cards, searchValue));
+
+}, false);
+
+self.addEventListener('transferobject', function(e) {
+
+    var json = JSON.parse(Encoding.UTF8.GetString(e.data));
+    var cards = json['data'].cards;
+    var searchValue = json['data'].search;
+
+    self.postMessage(doSearch(cards, searchValue));
+
+}, false);
+
+/**
+ * Search a card database, return all cards matching a specific
+ * @param cards
+ * @param searchValue
+ * @returns {Array}
+ */
+function doSearch(cards, searchValue) {
     var result = [];
 
     var i = 0;
@@ -22,6 +43,5 @@ self.addEventListener('message', function(e) {
         });
     });
 
-    self.postMessage(result);
-
-}, false);
+    return result;
+}
